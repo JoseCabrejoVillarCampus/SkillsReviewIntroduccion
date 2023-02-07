85e37a94a9829453f96e845c58f729c6e2b91662
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-02-07 10:52:38 -0500

    Review

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+"""1. Qué operadores utiliza Python en los siguientes casos:
+        A. División Modular
+        B. Exponenciación
+        C. División que retorne entero."""
+
+num1=3
+num2=2
+print(f"B. Exponenciacion {num1}**{num2}={num1**num2}")
+print(f"B. Exponenciacion {num1}//{num2}={num1//num2}")
+print(f"B. Exponenciacion {num1}%{num2}={num1%num2}")
