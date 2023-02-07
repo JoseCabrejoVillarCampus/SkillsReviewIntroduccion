7fa8b9efa7d2eae96257f44065896a4a46ffac97
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-02-07 13:20:48 -0500

    jose

--- ejercicio10.py
+++ ejercicio10.py
@@ -4,3 +4,21 @@ energía eléctrica. Los datos que se conocen son los
 siguientes:
     - Mes de consumo - Valor kw
     - Total kw consumido en el mes - estrato"""
+
+valorEnergia={'MesConsumo':'','estrato':'','valorKW':'','totalConsumido':''}
+
+valorEnergia['MesConsumo']=input("Ingrese Mes a Liquidar: ")
+valorEnergia['estrato']=input("Ingrese el estrato de su hogar: ")
+if valorEnergia['estrato'] == 1:
+    valorEnergia['valorKW']= 86
+elif valorEnergia['estrato'] == 2:
+    valorEnergia['valorKW']= 98
+elif valorEnergia['estrato'] == 3:
+    valorEnergia['valorKW']= 100
+elif valorEnergia['estrato'] == 4:
+    valorEnergia['valorKW']= 123
+elif valorEnergia['estrato'] == 5:
+    valorEnergia['valorKW']= 145
+elif valorEnergia['estrato'] == 6:
+    valorEnergia['valorKW']= 200
+
